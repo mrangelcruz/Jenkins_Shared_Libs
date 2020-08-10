6db424d94124ef5d87bc23b509f2c4132c523728
Angel Cruz <mrangelcruz@gmail.com>
2020-08-10 13:46:07 -0500

    deploy to qa

--- accountusage/pipeline_config.groovy
+++ accountusage/pipeline_config.groovy
@@ -1,11 +1,17 @@
 libraries{
     artifactRepo
     buildSteps
+    deploy_to_qa
 }
 
 application_environments{
     merge = true
-        server{
+        dev{
+            server_ip = "10.10.3.208"
+            branch = "master"
+            artifact_repo = "nexus"
+        }
+        qa{
             server_ip = "10.10.3.209"
             branch = "qa"
             artifact_repo = "folder"
