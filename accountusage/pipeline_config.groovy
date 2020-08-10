189c77db94bfc9ce60d209dc480c4913ebeaa2c9
Angel Cruz <mrangelcruz@gmail.com>
2020-08-09 22:38:32 -0500

    qa environment

--- accountusage/pipeline_config.groovy
+++ accountusage/pipeline_config.groovy
@@ -6,8 +6,8 @@ libraries{
 application_environments{
     merge = true
         server{
-            server_ip = "10.10.3.208"
-            branch = "master"
+            server_ip = "10.10.3.209"
+            branch = "qa"
             artifact_repo = "folder"
         }
 }
