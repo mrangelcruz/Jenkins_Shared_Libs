e1f7d9fc75ebc50a137c591a6360f5ab7e30a3da
mrangelcruz <mrangelcruz@gmail.com>
2020-08-11 07:12:50 -0500

    Update notify.groovy
    echo the branch we're in

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -3,6 +3,8 @@ def call(Map config=[:]) {
     if (config.type == "slack") {
         echo Constants.SLACK_MESSAGE
         echo config.message
+        echo 'Branch:'
+        echo config.BRANCH
     } else {
         echo Constants.EMAIL_MESSAGE
         echo config.message
