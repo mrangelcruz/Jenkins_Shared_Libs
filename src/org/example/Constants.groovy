37da1fcc226635487245b121cf2b070bee615329
mrangelcruz <mrangelcruz@gmail.com>
2020-08-11 07:08:10 -0500

    Update Constants.groovy
    adding Branch

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -2,4 +2,5 @@ package org.example
 class Constants {
   static final String SLACK_MESSAGE = "Sending Slack Notification"
   static final String EMAIL_MESSAGE = "Sending Email"
+  static final String BRANCH = "master"
 }
