f179cd84af78c3a7e9ea0060407c3889eca725ff
Angel Cruz <mrangelcruz@gmail.com>
2020-08-12 22:11:32 -0500

    qa branch

--- src/org/constants/Constants.groovy
+++ src/org/constants/Constants.groovy
@@ -2,5 +2,5 @@ package org.constants
 class Constants {
   static final String SLACK_MESSAGE = "Sending Slack Notification"
   static final String EMAIL_MESSAGE = "Sending Email"
-  static final String BRANCH = "master"
+  static final String BRANCH = "qa"
 }
